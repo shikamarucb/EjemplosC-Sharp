c8ee4a5d8c03cb4c0e0054b4700922e1ef88e06a
Andres Camacho <cbandresf@hotmail.com>
2018-01-22 11:52:33 -0500

    Desarrollo del ejemplo DateTime

--- DateTimeEj/Program.cs
+++ DateTimeEj/Program.cs
@@ -10,6 +10,29 @@ namespace DateTimeEj
     {
         static void Main(string[] args)
         {
+            //Fecha con hora
+            DateTime fechaHora = new DateTime(2017, 10, 30, 23, 12, 15);
+            Console.WriteLine(fechaHora);
+
+            //Fecha
+            DateTime fecha = new DateTime(1980,1,1);
+            Console.WriteLine(fecha);
+
+            //Impresion con formato
+            Console.WriteLine(fecha.ToString("dd / yyyy / MMMM"));
+            Console.WriteLine(fechaHora.ToString("dd / yyyy / MMMM /hh:mm:ss"));
+
+            //Añadir dias
+            fecha = fecha.AddDays(30);
+            Console.WriteLine("Nueva fecha: " + fecha);
+
+            //Dia de la semana
+            Console.WriteLine(fecha.DayOfWeek);
+
+            //Restar fechas
+            Console.WriteLine(fechaHora.Subtract(fecha).Days);
+
+            Console.Read();
         }
     }
 }
