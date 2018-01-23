b7ac9f7f370c99bc1471ac830f9c4c04647f593d
Andres Camacho <cbandresf@hotmail.com>
2018-01-22 22:21:02 -0500

    Ejemplo con enums

---
+++ EnumsEj/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsEj
+{
+    class Program
+    {
+        enum estados {
+            pendiente=1,
+            procesando=2,
+            completado=3,
+            eliminado=4
+        };
+
+        static void Main(string[] args)
+        {
+            //los enums evitan la manipulacion de estados fantasma, si deseo
+            //cambiar el valor de algun estado, lo hago en el enum no mas
+
+            int estado_de_la_operacion = 2;
+
+            if (estado_de_la_operacion == (int)estados.pendiente)
+                Console.WriteLine("Esta pendiente la informacion");
+            else if (estado_de_la_operacion == (int)estados.procesando)
+                Console.WriteLine("Se esta procesando la informacion");
+            else if (estado_de_la_operacion == (int)estados.completado)
+                Console.WriteLine("Se ha procesado la informacion");
+            else if (estado_de_la_operacion == (int)estados.eliminado)
+                Console.WriteLine("Se ha eliminado la informacion");
+
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+}
