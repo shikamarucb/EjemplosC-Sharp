542e61ceaaf420b2d1e31a0063a00434bf5e2d72
Andres Camacho <cbandresf@hotmail.com>
2018-01-23 22:56:56 -0500

    Ejemplo de comentarios

---
+++ Soluciones/Comentarios/ComentariosClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreEspacio
+{
+    /// <summary>
+    /// Esta es una clase para aprender a usar los comentarios
+    /// </summary>
+    class ComentariosClass
+    {
+        /// <summary>
+        /// Suma dos valores enteros
+        /// </summary>
+        /// <param name="a">sumando 1</param>
+        /// <param name="b">sumando 2</param>
+        /// <returns>sumando 1 + sumando 2</returns>
+        public static int metodo(int a, int b) {
+            return a + b;
+        }
+    }
+}
---
+++ Soluciones/Comentarios/Program.cs
@@ -0,0 +1,21 @@
+﻿using NombreEspacio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ComentariosClass.metodo(5, 5));
+
+            Console.WriteLine("\nFin del programa");
+            Console.Read();
+        }
+    }
+}
