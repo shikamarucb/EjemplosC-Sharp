234c2d0861e9dd7cc5dfa28fd830c818c65eab61
Andres Camacho <cbandresf@hotmail.com>
2018-01-25 00:00:34 -0500

    Metodos de Extension

---
+++ Soluciones/Metodos De Extension/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_De_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5.ElevadoALa(2,2));
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+
+    //Por Convencion usar un nombre similar
+    public static class IntegerExtensionMethods {
+
+        public static double ElevadoALa(this int valor, int n)
+        {
+            return Math.Pow(valor, n);
+        }
+
+        public static double ElevadoALa(this int valor, int n, int x)
+        {
+            return Math.Pow(valor, n+x);
+        }
+    }
+}
