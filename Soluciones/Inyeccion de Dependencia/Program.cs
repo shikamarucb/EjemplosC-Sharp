c37cf980c5b54c3745530fdfd25c72b6db8bed8c
Andres Camacho <cbandresf@hotmail.com>
2018-01-29 21:07:27 -0500

    Inyeccion de Dependencias
    Ejemplo de como implementar y usar una clase para manipular diferentes clases que heredan de una interfaz y asi eliminar las dependencia a una sola clase

---
+++ Soluciones/Inyeccion de Dependencia/Program.cs
@@ -0,0 +1,65 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inyeccion_de_Dependencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Perro puede cambiarse por cualquier clase que implemente la interfaz animal
+            var animal = new Perro();
+            var ruido = new Ruido(animal);
+            ruido.HaciendoRuido("Alberto");
+
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+
+        }
+    }
+
+    //La clase ruido elimina las dependencias de clase, yo puedo hacer que cualquier animal
+    //ejecute el metodo HacerRuido simplemente cambiando la instancia de la variable animal(var animal)
+    //en el metodo Main
+    class Ruido {
+
+        private Animal _animal;
+
+        public Ruido(Animal animal)
+        {
+            _animal = animal;
+        }
+
+        public void HaciendoRuido(string Name) {
+            Console.WriteLine(_animal.HacerRuido(Name));
+        }
+    }
+}
+
+namespace Interfaces
+{
+    interface Animal
+    {
+        string HacerRuido(string Name);
+    }
+
+    class Tigre : Animal
+    {
+        public string HacerRuido(string Name)
+        {
+            return Name + " : Grrrr!!";
+        }
+    }
+
+    class Perro : Animal
+    {
+        public string HacerRuido(string Name)
+        {
+            return Name + " : Woof Woof!!";
+        }
+    }
+}
