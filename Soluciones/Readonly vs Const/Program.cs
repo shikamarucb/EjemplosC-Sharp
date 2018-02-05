76ea97c1769c0ad9b22011f5eda16a5c03a9f7a4
Andres Camacho <cbandresf@hotmail.com>
2018-02-05 18:52:27 -0500

    readonly vs const

---
+++ Soluciones/Readonly vs Const/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Readonly_vs_Const
+{
+    class Program
+    {
+        //readonly y const sirven para definir valores que no varían. La diferencia radica en que readonly puede ser definido en tiempo de ejecución, 
+        //mientras que const debe ser definido en tiempo de compilación.
+
+
+        const double PI = 3.14;
+        static readonly double PI_2 = RepositorioValores.PI;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PI);
+            Console.WriteLine(PI_2);
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+
+        }
+    }
+
+    class RepositorioValores {
+        public static double PI = 3.14159;
+    }
+}
