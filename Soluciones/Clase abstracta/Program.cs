14af7ff06ffa217867b6bcebfcbdd432a6287981
Andres Camacho <cbandresf@hotmail.com>
2018-02-05 17:56:44 -0500

    Clase Abstracta

---
+++ Soluciones/Clase abstracta/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_abstracta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var perro = new Perro();
+            Animal animal;
+            perro.Nombre = "Benji";
+            Console.WriteLine(perro.Nombre);
+            perro.ObtenerEdad();
+            animal = perro;
+            Console.WriteLine(animal.ObtenerEdad());
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+
+    //La clase abstracta no se puede instanciar
+    abstract class Animal
+    {
+        private int Edad=15;
+        public string Nombre { get; set; }
+        public virtual int ObtenerEdad()
+        {
+            return Edad;
+        }
+    }
+
+    class Perro:Animal
+    {
+        public new void ObtenerEdad()
+        {
+            Console.WriteLine("Upss!");
+        }
+    }
+}
