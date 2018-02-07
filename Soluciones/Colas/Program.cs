a35d4aa1f891aab8bb293d8dc868f426c2df2d45
Andres Camacho <cbandresf@hotmail.com>
2018-02-06 21:27:56 -0500

    Colas

--- Soluciones/Colas/Program.cs
+++ Soluciones/Colas/Program.cs
@@ -10,6 +10,37 @@ namespace Colas
     {
         static void Main(string[] args)
         {
+            Queue<int> cola = new Queue<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                //agrega elementos a la cola
+                cola.Enqueue(i);
+            }
+            foreach (var i in cola)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Quita elementos de la cola
+                Console.WriteLine(cola.Dequeue());
+            }
+
+            Console.WriteLine();
+
+            foreach (var i in cola)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+
+
         }
     }
 }
