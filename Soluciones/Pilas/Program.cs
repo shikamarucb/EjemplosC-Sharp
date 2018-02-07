c410a416fe7321d438336995a7282e6b124f7d88
Andres Camacho <cbandresf@hotmail.com>
2018-02-06 21:31:47 -0500

    Pilas

---
+++ Soluciones/Pilas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numeros = new Stack<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                //agrega numeros a la pila
+                numeros.Push(i);
+            }
+
+            foreach (var i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                //elimina numeros de la pila
+                numeros.Pop();
+            }
+
+            Console.WriteLine();
+
+            foreach (var i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+}
