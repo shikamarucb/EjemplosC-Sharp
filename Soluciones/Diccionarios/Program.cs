2cd5b4ffbb1cd20ed594a7bf9d84d1bdd82b3c14
Andres Camacho <cbandresf@hotmail.com>
2018-02-06 20:45:21 -0500

    Diccionarios

---
+++ Soluciones/Diccionarios/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> persona = new Dictionary<int, string>();
+            persona.Add(1, "Andres");
+            persona.Add(2, "Felipe");
+            persona.Add(3, "Camacho");
+            persona.Add(4, "Barragan");
+
+            Console.WriteLine(persona[1]);
+
+            if (persona.ContainsKey(5))
+            {
+                Console.WriteLine(persona[5]);
+            }
+
+            persona.Remove(2);
+
+            foreach (var keys in persona.Keys)
+            {
+                Console.WriteLine(keys);
+            }
+
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+
+        }
+    }
+}
