b15ef4a0be0adfdff9fb61b31f2286d09c8d25f1
Andres Camacho <cbandresf@hotmail.com>
2018-02-06 23:29:06 -0500

    Arreglos de Arreglos

---
+++ Soluciones/Arreglos de Arreglos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos_de_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[2][];
+            array[0] = new int[4];
+            array[0][0] = 5;
+            array[0][1] = 3;
+            array[0][2] = 4;
+            array[0][3] = 3;
+            array[1] = new int[]
+            {
+                1,2,5,7,8,23,1
+            };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                var join = string.Join(",", array[i]);
+                Console.WriteLine(join);
+            }
+
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+}
