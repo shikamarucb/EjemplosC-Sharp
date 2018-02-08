03ce4b6b919bb2414a1904d1183048ba70567d86
Andres Camacho <cbandresf@hotmail.com>
2018-02-08 18:32:38 -0500

    Expresiones Lambda

---
+++ Soluciones/Expresiones Lambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, double> operacion = (x, y) =>
+                {
+                    x = (int)Math.Pow(x, 2);
+                    y = (int)Math.Pow(y, 2);
+                    return x + y;
+                };
+
+            Console.WriteLine(operacion(2,3));
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+}
