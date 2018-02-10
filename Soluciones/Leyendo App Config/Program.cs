639baddbcf215bf132c8d598122f287525f14929
Andres Camacho <cbandresf@hotmail.com>
2018-02-09 23:23:05 -0500

    Leyendo el archivo App.config

---
+++ Soluciones/Leyendo App Config/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leyendo_App_Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mensaje = ConfigurationManager.AppSettings["mensaje"];
+            Console.WriteLine(mensaje);
+            Console.WriteLine("\nFin del Programa");
+            Console.Read();
+        }
+    }
+}
