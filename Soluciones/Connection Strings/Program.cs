918ffa4b53356737f63b53f9cf15f98102828214
Andres Camacho <cbandresf@hotmail.com>
2018-02-10 01:06:06 -0500

    Connection Strings

---
+++ Soluciones/Connection Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-HEKH212";
+            builder.InitialCatalog = "Prueba2";
+
+            //para usar windows authentication poner true y quitar UserID y Password
+            builder.IntegratedSecurity = false;
+            builder.UserID = "AndresK2";
+            builder.Password = "941215";
+
+            var connectionString = builder.ToString();*/
+
+            var connectionString = ConfigurationManager.ConnectionStrings["cualquierNombre"].ConnectionString;
+
+            using(SqlConnection sql = new SqlConnection(connectionString))
+            {
+                sql.Open();
+            }
+
+        }
+    }
+}
