f0809db3ec32c6d4117a89c865bfa00d24397b9d
Andres Camacho <cbandresf@hotmail.com>
2018-02-12 22:14:29 -0500

    Ejecutar Query desde C#

---
+++ Soluciones/Ejecutar Query Sql ADO/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejecutar_Query_Sql_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["cualquierNombre"].ConnectionString;
+
+            var IdPais = 5;
+
+            var query = @"select *
+                        from Personas
+                        where IdPais = @IdPais";
+
+            //Console.WriteLine(query);
+
+            using (SqlConnection sql = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sql))
+                {
+                    //Reemplaza @IdPais por el valor de la varible IdPais
+                    cmd.Parameters.AddWithValue("@IdPais", IdPais);
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    sql.Open();
+                    da.Fill(dt);
+                }
+            }
+
+            /*
+             * ESTE QUERY ES SUCEPTIBLE A UN ATAQUE DE INYECCION DE CODIGO SQL!!!!!
+             * 
+            var query = @"select *
+                        from Personas
+                        where IdPais = " + IdPais;
+            
+             
+
+            using (SqlConnection sql = new SqlConnection(connectionString))
+            {
+                using(SqlCommand cmd = new SqlCommand(query, sql))
+                {
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    sql.Open();
+                    da.Fill(dt);
+                }
+            }
+            
+            */
+
+        }
+    }
+}
