a7e07f4c22dc1b5d3e7b899e23f5df2a1608e7d3
Andres Camacho <cbandresf@hotmail.com>
2018-02-14 23:01:42 -0500

    Funciones Locales

---
+++ Soluciones/Funciones Locales C-SH7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_Locales_C_SH7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Funcion1()
+            {
+                Console.WriteLine("Hola desde Funcion1");
+
+                int Funcion2(int a, int b)
+                {
+                    return a + b;
+                }
+
+                Console.WriteLine("Hola desde Funcion 2, 5+7:"+Funcion2(5,7));
+            }
+
+            Funcion1();
+
+            Funcion3();
+
+            void Funcion3()
+            {
+                Console.WriteLine("Hola desde funcion 3");
+            }
+
+            int Multiplicar(int x, int y) => x * y;
+
+            Console.WriteLine("Hola desde Expresion Lambda: "+Multiplicar(5,7));
+
+            Console.Read();
+        }
+    }
+}
