324707978dd53a4d55f9f15b461f87c2898c07e3
Andres Camacho <cbandresf@hotmail.com>
2018-02-15 00:00:51 -0500

    Expresiones en una clase C-SH7

---
+++ Soluciones/Expresiones en una clase C-SH7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones_en_una_clase_C_SH7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado c = new Cuadrado();
+            c.MyProperty = 3;
+
+            Console.Read();
+        }
+    }
+
+    class Cuadrado
+    {
+        public Cuadrado()
+        {
+
+        }
+
+        public Cuadrado(int dimension) => AsignarDimension(dimension);
+
+        private void AsignarDimension(int dimension)
+        {
+            Altura = dimension;
+        }
+
+        public int Altura { get; set; }
+
+        public int funcion()
+        {
+            return 1;
+        }
+
+        public int MyProperty {
+            get=>funcion();
+            set =>AsignarDimension(value);
+        }
+
+        ~Cuadrado()=> Destruir();
+
+        private void Destruir()
+        {
+
+        }
+    }
+}
