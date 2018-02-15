557adabbea310b5c0bbbc2ef8cb45af10fd6a6ab
Andres Camacho <cbandresf@hotmail.com>
2018-02-15 00:00:11 -0500

    Separador de digitos C# 7

---
+++ Soluciones/Separador de digitos C-SH7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Separador_de_digitos_C_SH7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sirve para que el codigo sea mas legible
+
+            //uso correcto
+            int n = 456_456;
+            double n2 = 456_456.12_12;
+
+            //uso incorrecto
+            //int n3 = _123;
+            //int n4 = 123_;
+            //double n5 = 123_.123;
+            //double n6 = 123._123;
+
+        }
+    }
+}
