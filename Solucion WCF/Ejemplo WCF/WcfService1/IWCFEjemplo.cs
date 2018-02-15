9f2a5940b42cf34f633821e1b1822d76bd3b5ebc
Andres Camacho <cbandresf@hotmail.com>
2018-02-14 22:19:53 -0500

    Ejemplo WCF

---
+++ Solucion WCF/Ejemplo WCF/WcfService1/IWCFEjemplo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    //Definicion del contrato del web service
+    [ServiceContract]
+    public interface IWCFEjemplo
+    {
+
+        //Operaciones o funciones del contrato
+        [OperationContract]
+        Persona ObtenerPersonas(int id);
+    }
+
+    //Datos del contrato
+    [DataContract]
+    public class Persona : BaseMensaje
+    {
+        //Parte de los datos del contrato
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public int Edad { get; set; }
+    }
+
+    [DataContract]
+    public class BaseMensaje
+    {
+        [DataMember]
+        public string Error { get; set; }
+    }
+}
---
+++ Solucion WCF/Ejemplo WCF/WcfService1/WCFEjemplo.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class WCFEjemplo : IWCFEjemplo
+    {
+        
+        public Persona ObtenerPersonas(int id)
+        {
+            if (id == 1)
+                return new Persona()
+                {
+                    Nombre = "Jaime",
+                    Edad = 23
+                };
+            else
+                return new Persona()
+                {
+                    Error = "El Id no corresponde con ninguna persona"
+                };
+        }
+    }
+}
