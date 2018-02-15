9d754336bf83d89a00fcf042f42e78475b97fbd2
Andres Camacho <cbandresf@hotmail.com>
2018-02-14 23:13:30 -0500

    Patron de igualacion is C# 7

---
+++ Soluciones/Patron de igualacion is/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patron_de_igualacion_is
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura figura1 = new Poligono();
+            Figura figura2 = new Cicunferencia();
+
+            //Antes de C# 7
+            var fg = figura1 as Poligono;
+
+            if(fg != null)
+            {
+                fg.nLados = 5;
+            }
+            //Despues de C# 7
+            if(figura1 is Poligono fg2)
+            {
+                fg2.nLados = 8;
+            }
+        }
+    }
+
+    class Figura
+    {
+
+    }
+
+    class Cicunferencia:Figura
+    {
+
+    }
+    class Poligono:Figura
+    {
+        public int nLados { get; set; }
+    }
+}
