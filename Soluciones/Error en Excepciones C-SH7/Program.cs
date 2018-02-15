c541a5444cd11cc844544586bb7f236140139790
Andres Camacho <cbandresf@hotmail.com>
2018-02-15 00:18:41 -0500

    Error en Expresiones C# 7

---
+++ Soluciones/Error en Excepciones C-SH7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Error_en_Excepciones_C_SH7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividir(int a, int b) => (b != 0) ? a / b : throw new Exception("Division entre cero");
+
+            dividir(5, 2);
+            dividir(5, 0);
+
+            Console.Read();
+        }
+    }
+}
